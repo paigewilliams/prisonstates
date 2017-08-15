afad987c42db3167ab8c5d9c1f6ee1c66aed9cc1
paigewilliams <paw145@humboldt.edu>
2017-08-15 15:13:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,7 @@ function initMap() {
    
    var DarkAll = new L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGF3MTQ1IiwiYSI6ImNpbXJlamZxNzAweGt1cmtrZ2Z2ZXZseXcifQ.1jrOxO5uFc6rWEyDKdI4dQ').addTo(map);
    
-   $.getJSON('states.geojson', function(data){
-     var geojson = LgeoJson(data, {
-       onEachFeature: function (feature, layer) {
-         (function(layer, properties) {
-         });
+   L.geoJson(states.js).addTo(map);
    
    }
    
